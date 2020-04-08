c7b7a987c765ba004863e5ec9505206bd297a33e
Leesw_0811 <s2018s15@e-mirim.hs.kr>
2020-04-08 19:25:40 +0900

    수정

--- bg.js
+++ bg.js
@@ -1,36 +1,26 @@
 const body = document.querySelector("body");
-const IMG_NUMBER = 10;
-const image = new Image();
-var imgNumber = 1; 
+const IMG_NUMBER = 3;
 
 function handleImgLoad(){
     console.log("finished load image");
 }
 
-function plus(){
-    if(imgNumber >= 10)
-        imgNumber = 0;
-
-    imgNumber+=1;
-}
-
-function paintImage(){
-    plus();
-    image.src = `img/MoonImg/${imgNumber}.png`;
-}
-
-// function genRandom(){
-//     const number = Math.floor(Math.random()*3);
-//     return number;
-// }
-
-function init(){
-    // const randomNumber = genRandom();
-    // paintImage(0);
-    image.src = `img/MoonImg/${imgNumber}.png`;
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src = `img/${imgNumber+1}.jpg`;
     image.classList.add("bgImage");
     body.prepend(image);
-    setInterval(paintImage, 2000);
+   
+}
+
+function genRandom(){
+    const number = Math.floor(Math.random()*3);
+    return number;
+}
+
+function init(){
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
 }
 
 init();
